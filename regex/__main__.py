62487f153c1959e4e77082aea43e30f8ab3890e2
Renato Avellar Nobre <rekanobre@gmail.com>
2020-06-08 14:00:35 -0300

    Testing branch rules

--- regex/__main__.py
+++ regex/__main__.py
@@ -9,8 +9,8 @@ from core import Regex
 if __name__ == "__main__":
     txts = helper.get_txts("./data/test/")
 
-    abo = Regex.get_act_obj("abono", txts[6])
-    print(abo.data_frame)
+    # abo = Regex.get_act_obj("abono", txts[6])
+    # print(abo.data_frame)
 
     all = Regex.get_all_df(txts[0])
     # print(all)
