0814ce5b040986a1c10873db5e641752183c1ee7
Khalil Carsten <khalilcarsten@gmail.com>
2020-06-16 23:25:04 -0300

    Regex de nomeação e Melhoria do notebook de análise

--- regex/atos/nomeacao.py
+++ regex/atos/nomeacao.py
@@ -15,16 +15,16 @@ class NomeacaoComissionados(Atos):
     def _rule_for_inst(self):
         start = r"(NOMEAR)"
         body = r"([\s\S]*?)"
-        end = "\."
+        end = r"\.\s"
         return start + body + end
     
     def _prop_rules(self):
-        rules = rules = {"nome": r"(^[A-ZÀ-Ž\s]+[A-ZÀ-Ž])",
-                         "cargo_efetivo": r"",
-                         "matricula": r"matr[í|i]cula\s?n?o?\s([\s\S]*?)[,|\s]",
-                         "siape": r"[S|s][I|i][A|a][P|p][E|e]\s[N|n]?[o|O]?\s([\s\S]*?)[,| | .]",
-                         "simbolo": r"[S|s][í|i]mbolo\s?n?o?\s([\s\S]*?)[,|\s]",
-                         "cargo_comissao": "",
-                         "lotacao": "",
-                         "orgao": ""}
+        rules = {"nome": r"(^[A-ZÀ-Ž\s]+[A-ZÀ-Ž])",
+                 "cargo_efetivo": r"(?:^[A-ZÀ-Ž\s]+[A-ZÀ-Ž]),\s(?!matricula)([\s\S]*?),\s",
+                 "matricula": r"matr[í|i]cula\s?n?o?\s([\s\S]*?)[,|\s]",
+                 "siape": r"[S|s][I|i][A|a][P|p][E|e]\s[N|n]?[o|O]?\s([\s\S]*?)[,| | .]",
+                 "simbolo": r"[S|s][í|i]mbolo\s?n?o?\s([\s\S]*?)[,|\s]",
+                 "cargo_comissao": r"(?:[S|s][í|i]mbolo\s?n?o?\s(?:[\s\S]*?)[,|\s])\sde([\s\S]*?),",
+                 "lotacao": r"(?:[S|s][í|i]mbolo\s?n?o?\s(?:[\s\S]*?)[,|\s])\sde(?:[\s\S]*?),\sd[a|e|o]\s([\s\S]*,?),",
+                 "orgao": r"(?:[S|s][í|i]mbolo\s?n?o?\s(?:[\s\S]*?)[,|\s])\sde(?:[\s\S]*?),\sd[a|e|o]\s(?:[\s\S]*,?),\sd[a|e|o]\s([\s\S]*?)$"}
         return rules
